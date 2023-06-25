cbb7c88c0b2f25b143f1d75730864de8d16b3344
Suleyman Taspinar <suleyman.taspinar@getir.com>
2023-06-25 19:49:03 +0300

    [feature] refactor dpk

--- dpk.js
+++ dpk.js
@@ -3,26 +3,23 @@ const crypto = require("crypto");
 exports.deterministicPartitionKey = (event) => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
-
-  if (event) {
-    if (event.partitionKey) {
-      candidate = event.partitionKey;
-    } else {
-      const data = JSON.stringify(event);
-      candidate = crypto.createHash("sha3-512").update(data).digest("hex");
-    }
+  
+  if (!event) {
+    return TRIVIAL_PARTITION_KEY;
   }
-
-  if (candidate) {
-    if (typeof candidate !== "string") {
-      candidate = JSON.stringify(candidate);
-    }
+  
+  const { partitionKey } = event;
+  
+  const shouldHashWholeObject = event && !event.partitionKey;
+  const shouldHashPartitionKey = partitionKey && partitionKey.length > MAX_PARTITION_KEY_LENGTH;
+  
+  if (shouldHashWholeObject) {
+    return crypto.createHash("sha3-512").update(JSON.stringify(event)).digest("hex");
+  } else if (shouldHashPartitionKey) {
+    return crypto.createHash("sha3-512").update(event.partitionKey).digest("hex");
+  } else if (typeof partitionKey === "string") {
+    return String(partitionKey);
   } else {
-    candidate = TRIVIAL_PARTITION_KEY;
+    return JSON.stringify(partitionKey);
   }
-  if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
-    candidate = crypto.createHash("sha3-512").update(candidate).digest("hex");
-  }
-  return candidate;
 };
